04897459ee52d59b1c92a8543f3cbde364979534
aayushghimire321 <aghimire491@gmail.com>
2024-12-03 10:21:31 +0545

    commit

--- lib/view/snackbar.dart
+++ lib/view/snackbar.dart
@@ -5,9 +5,57 @@ class SnackBarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _showSnackbar(){
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('hello world')));
+    _showSnackbar() {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Show Message 1',
+            style: TextStyle(color: Colors.white),
+          ),
+          duration: Duration(seconds: 2),
+          backgroundColor: Colors.green,
+        ),
+      );
     }
-    return const Placeholder();
+
+    _showSnackbar1() {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Show Message 2',
+            style: TextStyle(color: Colors.white),
+          ),
+          duration: Duration(seconds: 2),
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        body: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _showSnackbar();
+                  },
+                  child: const Text('Show Message 1'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _showSnackbar1();
+                  },
+                  child: const Text('Show Message 2'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
