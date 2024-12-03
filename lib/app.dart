bbb5870a57a7bb04427664e21ccb86e4e94c0749
aayushghimire321 <aghimire491@gmail.com>
2024-12-03 09:45:27 +0545

    commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 
+import 'package:first_flutter_project/view/flexible_expended_view.dart';
 import 'package:flutter/material.dart';
 
 import 'view/image.dart';
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return    const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:LoadImageView(),
+      home:flexible(),
     );
   }
 }
--- lib/view/flexible_expended_view.dart
+++ lib/view/flexible_expended_view.dart
@@ -5,6 +5,22 @@ class flexible extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+        Container(
+          height: 700,
+          width: double.infinity,
+          color: Colors.yellow,
+        ),
+          Flexible(
+            child: Container(
+              height: 300,
+              color: Colors.black,
+            ),
+          ),
+      ],
+      ),
+    );
   }
 }
