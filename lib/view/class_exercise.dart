6a8f34f8ba3a4d721099893e362814d74dcc1c6c
aayushghimire321 <aghimire491@gmail.com>
2024-12-03 10:55:41 +0545

    commit

--- lib/view/class_exercise.dart
+++ lib/view/class_exercise.dart
@@ -11,69 +11,85 @@ class Classwork extends StatelessWidget {
         body: Column(
           children: [
             // Row at the top
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(16),
+            Expanded(
+              flex: 1, // Assign relative weight to the row
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                      child: Center(
+                        child: Text(
+                          'Container 1',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                      child: Center(
+                        child: Text(
+                          'Container 2',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                      child: Center(
+                        child: Text(
+                          'Container 3',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // First container
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.blue,
+                margin: EdgeInsets.all(4),
+                child: Center(
                   child: Text(
                     'Container 1',
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-                Container(
-                  color: Colors.red,
-                  padding: EdgeInsets.all(16),
+              ),
+            ),
+            // Second container
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.blue,
+                margin: EdgeInsets.all(4),
+                child: Center(
                   child: Text(
                     'Container 2',
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-                Container(
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(16),
+              ),
+            ),
+            // Third container
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.blue,
+                margin: EdgeInsets.all(4),
+                child: Center(
                   child: Text(
                     'Container 3',
                     style: TextStyle(color: Colors.white),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16), // Space between rows and column
-            // First container
-            Container(
-              color: Colors.blue,
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.all(32),
-              child: Center(
-                child: Text(
-                  'Container 1',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            // Second container
-            Container(
-              color: Colors.blue,
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.all(32),
-              child: Center(
-                child: Text(
-                  'Container 2',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            // Third container
-            Container(
-              color: Colors.blue,
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.all(32),
-              child: Center(
-                child: Text(
-                  'Container 3',
-                  style: TextStyle(color: Colors.white),
                 ),
               ),
             ),
